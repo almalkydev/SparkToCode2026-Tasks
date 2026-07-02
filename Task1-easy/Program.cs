a1f4b3e5f7f52bb4bab8edeed08877ec0958cd13
ahmedalmalky18 <88257232+9ozz@users.noreply.github.com>
2026-07-02 09:06:22 +0400

    Update Program.cs

--- Task1-easy/Program.cs
+++ Task1-easy/Program.cs
@@ -29,3 +29,14 @@ double width = double.Parse(Console.ReadLine());
 Console.WriteLine("Area: " + (length * width));
 Console.WriteLine("Perimeter: " + (2 * (length + width)));
 
+// Task 3 - Rectangle Calculation
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("Even");
+else
+    Console.WriteLine("Odd");
+
+
