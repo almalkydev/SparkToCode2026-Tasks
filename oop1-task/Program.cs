a14843d63e10433b575779bab55677a9586e077d
ahmedalmalky18 <88257232+9ozz@users.noreply.github.com>
2026-07-13 11:06:50 +0400

    Add BankAccount constructors

--- oop1-task/Program.cs
+++ oop1-task/Program.cs
@@ -5,3 +5,12 @@ class BankAccount
     public int AccountNumber;
     public string HolderName;
     public double Balance;
+    public BankAccount() { }
+
+    // Case 16 - parameterized constructor
+    public BankAccount(int accNum, string holder, double balance)
+    {
+        AccountNumber = accNum;
+        HolderName = holder;
+        Balance = balance;
+    }
